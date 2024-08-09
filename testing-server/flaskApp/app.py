d6461371209639976e86668a8352ed0b14613d3d
SimonMobi <85573550+SimonMobi@users.noreply.github.com>
2024-08-09 11:40:13 +0100

    update for cors, pull and run requirements.txt

--- testing-server/flaskApp/app.py
+++ testing-server/flaskApp/app.py
@@ -1,17 +1,18 @@
+from flask_cors import CORS
 from flask import Flask
 from flask import render_template
 from decouple import config
 
 app = Flask(__name__)
-
+CORS(app)
 
 @app.route('/')
 def hello_world():  # put application's code here
     return render_template(
         'index.html',
-        url_to_test=config("WIDGET_SERVER_URL", default="prod-widget-discover-uni.azurewebsites.net/")
+        url_to_test=config("WIDGET_SERVER_URL")
     )
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=config("FLASK_DEBUG", default=False))
